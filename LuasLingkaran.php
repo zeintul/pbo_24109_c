472795a25fbd2e146d38b4e5894bf27a50439283
zeintul <if24.lutfisaepulah@mhs.ubpkarawang.ac.id>
2025-11-07 15:43:54 +0700

    Pertemuan 4 - update LuasLingkaran (inheritance)

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -4,10 +4,20 @@ class LuasLingkaran {
 
     public const phi = 3.14;
     public int $jari;
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "<br/>";
+        echo "ini dari static";
+    }
 }
 
 $lingkaran = new LuasLingkaran();
 $lingkaran->jari = 12;
+$lingkaran->tampil('roda'); //panggil method tampil
 
-$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
-echo "Hasilnya adalah: ".$rumus;
+LuasLingkaran::testing(); //panggil static method
