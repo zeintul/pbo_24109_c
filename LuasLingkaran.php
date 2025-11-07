5e112d0d8f3853f399948a82eb487c8ee8d3b168
zeintul <if24.lutfisaepulah@mhs.ubpkarawang.ac.id>
2025-11-07 15:42:06 +0700

    update LuasLingkaran pertemuan3 (revisi)

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,28 +1,13 @@
 <?php
 
-namespace App\Math;
-
 class LuasLingkaran {
 
     public const phi = 3.14;
-    protected int $jari;
-    
-    public function __construct($isiJari = 1) {
-        $this->jari = $isiJari; //10
-    }
-
-    public function tampil($nama = 'ban') {
-        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
-        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
-    }
-
-    public static function testing() {
-        echo "<br/>";
-        echo "ini dari static";
-    }
-
-    public function __destruct() {
-        echo "<br/>";
-        echo "udah ah cape";
-    }
+    public int $jari;
 }
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 12;
+
+$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+echo "Hasilnya adalah: ".$rumus;
