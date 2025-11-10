52e2028c19f39ae0b60bd3c032a40e552d68a72a
zeintul <if24.lutfisaepulah@mhs.ubpkarawang.ac.id>
2025-11-10 23:42:31 +0700

    main

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,9 +1,15 @@
 <?php
 
+namespace App\Math;
+
 class LuasLingkaran {
 
     public const phi = 3.14;
-    public int $jari;
+    protected int $jari;
+    
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari; //10
+    }
 
     public function tampil($nama = 'ban') {
         $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
@@ -14,10 +20,9 @@ class LuasLingkaran {
         echo "<br/>";
         echo "ini dari static";
     }
+
+    public function __destruct() {
+        echo "<br/>";
+        echo "udah ah cape";
+    }
 }
-
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 12;
-$lingkaran->tampil('roda'); //panggil method tampil
-
-LuasLingkaran::testing(); //panggil static method
